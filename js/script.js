23b415f73edd0f60ffae47bb875064b49c3de017
MatiM72737 <72034316+MatiM72737@users.noreply.github.com>
2023-06-05 19:04:48 +0200

    cm

--- js/script.js
+++ js/script.js
@@ -1,72 +1,84 @@
-function ONAS()
-{
-    var ONAS = document.getElementById("ONAS");
-    var KEBABY = document.getElementById("KEBABY");
-    var ZAMOWIENIA = document.getElementById("ZAMOWIENIA");
-    var KONTAKT = document.getElementById("KONTAKT");
-    ONAS.classList.remove("none");
-    ONAS.classList.add("contentClass");
+function ONAS() {
+  var ONAS = document.getElementById("ONAS");
+  var KEBABY = document.getElementById("KEBABY");
+  var ZAMOWIENIA = document.getElementById("ZAMOWIENIA");
+  var KONTAKT = document.getElementById("KONTAKT");
+  ONAS.classList.remove("none");
+  ONAS.classList.add("contentClass");
 
-    KEBABY.classList.remove("contentClass");
-    KEBABY.classList.add("none");
+  KEBABY.classList.remove("contentClass");
+  KEBABY.classList.add("none");
 
-    ZAMOWIENIA.classList.remove("contentClass");
-    ZAMOWIENIA.classList.add("none");
+  ZAMOWIENIA.classList.remove("contentClass");
+  ZAMOWIENIA.classList.add("none");
 
-    KONTAKT.classList.remove("contentClass");
-    KONTAKT.classList.add("none"); 
+  KONTAKT.classList.remove("contentClass");
+  KONTAKT.classList.add("none");
+  const mysql = require("mysql");
+  const connection = mysql.createConnection({
+    host: "db4free.net",
+    user: "jk_root",
+    password: "uZnvM42367",
+    database: "test",
+  });
+  connection.connect((err) => {
+    console.log("jol");
+    if (err) throw err;
+    alert("Connected to the database!");
+  });
+  connection.end((err) => {
+    if (err) throw err;
+    console.log("Disconnected from the database!");
+  });
 }
-function KEBABY()
-{
-    var ONAS = document.getElementById("ONAS");
-    var KEBABY = document.getElementById("KEBABY");
-    var ZAMOWIENIA = document.getElementById("ZAMOWIENIA");
-    var KONTAKT = document.getElementById("KONTAKT");
-    KEBABY.classList.remove("none");
-    KEBABY.classList.add("contentClass");
+function KEBABY() {
+  var ONAS = document.getElementById("ONAS");
+  var KEBABY = document.getElementById("KEBABY");
+  var ZAMOWIENIA = document.getElementById("ZAMOWIENIA");
+  var KONTAKT = document.getElementById("KONTAKT");
+  KEBABY.classList.remove("none");
+  KEBABY.classList.add("contentClass");
 
-    ONAS.classList.remove("contentClass");
-    ONAS.classList.add("none");
+  ONAS.classList.remove("contentClass");
+  ONAS.classList.add("none");
 
-    ZAMOWIENIA.classList.remove("contentClass");
-    ZAMOWIENIA.classList.add("none");
+  ZAMOWIENIA.classList.remove("contentClass");
+  ZAMOWIENIA.classList.add("none");
 
-    KONTAKT.classList.remove("contentClass");
-    KONTAKT.classList.add("none");
+  KONTAKT.classList.remove("contentClass");
+  KONTAKT.classList.add("none");
 }
-function ZAMOWIENIA()
-{
-    var ONAS = document.getElementById("ONAS");
-    var KEBABY = document.getElementById("KEBABY");
-    var ZAMOWIENIA = document.getElementById("ZAMOWIENIA");
-    var KONTAKT = document.getElementById("KONTAKT");
-    ZAMOWIENIA.classList.remove("none");
-    ZAMOWIENIA.classList.add("contentClass");
+function ZAMOWIENIA() {
+  var ONAS = document.getElementById("ONAS");
+  var KEBABY = document.getElementById("KEBABY");
+  var ZAMOWIENIA = document.getElementById("ZAMOWIENIA");
+  var KONTAKT = document.getElementById("KONTAKT");
+  ZAMOWIENIA.classList.remove("none");
+  ZAMOWIENIA.classList.add("contentClass");
 
-    KEBABY.classList.remove("contentClass");
-    KEBABY.classList.add("none");
+  KEBABY.classList.remove("contentClass");
+  KEBABY.classList.add("none");
 
-    ONAS.classList.remove("contentClass");
-    ONAS.classList.add("none");
+  ONAS.classList.remove("contentClass");
+  ONAS.classList.add("none");
 
-    KONTAKT.classList.remove("contentClass");
-    KONTAKT.classList.add("none");
+  KONTAKT.classList.remove("contentClass");
+  KONTAKT.classList.add("none");
 }
-function KONTAKT()
-{
-    var ONAS = document.getElementById("ONAS");
-    var KEBABY = document.getElementById("KEBABY");
-    var ZAMOWIENIA = document.getElementById("ZAMOWIENIA");
-    var KONTAKT = document.getElementById("KONTAKT");
-    KONTAKT.classList.remove("none");
-    KONTAKT.classList.add("contentClass");
+function KONTAKT() {
+  var ONAS = document.getElementById("ONAS");
+  var KEBABY = document.getElementById("KEBABY");
+  var ZAMOWIENIA = document.getElementById("ZAMOWIENIA");
+  var KONTAKT = document.getElementById("KONTAKT");
+  KONTAKT.classList.remove("none");
+  KONTAKT.classList.add("contentClass");
 
-    KEBABY.classList.remove("contentClass");
-    KEBABY.classList.add("none");
+  KEBABY.classList.remove("contentClass");
+  KEBABY.classList.add("none");
 
-    ZAMOWIENIA.classList.remove("contentClass");
-    ZAMOWIENIA.classList.add("none");
+  ZAMOWIENIA.classList.remove("contentClass");
+  ZAMOWIENIA.classList.add("none");
 
-    ONAS.classList.remove("contentClass");
-    ONAS.classList.add("none");
+  ONAS.classList.remove("contentClass");
+  ONAS.classList.add("none");
 }
