06f06102d80f38b13b95d05c2ed47a75322476ce
MatiM72737 <72034316+MatiM72737@users.noreply.github.com>
2023-06-05 19:23:03 +0200

    cm

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const port = 80 || process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
