7e0e17f8e2778f5ed2a221691dfa26684dbfdb01
MatiM72737 <72034316+MatiM72737@users.noreply.github.com>
2023-06-05 15:43:25 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,72 @@
+function ONAS()
+{
+    var ONAS = document.getElementById("ONAS");
+    var KEBABY = document.getElementById("KEBABY");
+    var ZAMOWIENIA = document.getElementById("ZAMOWIENIA");
+    var KONTAKT = document.getElementById("KONTAKT");
+    ONAS.classList.remove("none");
+    ONAS.classList.add("contentClass");
+
+    KEBABY.classList.remove("contentClass");
+    KEBABY.classList.add("none");
+
+    ZAMOWIENIA.classList.remove("contentClass");
+    ZAMOWIENIA.classList.add("none");
+
+    KONTAKT.classList.remove("contentClass");
+    KONTAKT.classList.add("none");
+}
+function KEBABY()
+{
+    var ONAS = document.getElementById("ONAS");
+    var KEBABY = document.getElementById("KEBABY");
+    var ZAMOWIENIA = document.getElementById("ZAMOWIENIA");
+    var KONTAKT = document.getElementById("KONTAKT");
+    KEBABY.classList.remove("none");
+    KEBABY.classList.add("contentClass");
+
+    ONAS.classList.remove("contentClass");
+    ONAS.classList.add("none");
+
+    ZAMOWIENIA.classList.remove("contentClass");
+    ZAMOWIENIA.classList.add("none");
+
+    KONTAKT.classList.remove("contentClass");
+    KONTAKT.classList.add("none");
+}
+function ZAMOWIENIA()
+{
+    var ONAS = document.getElementById("ONAS");
+    var KEBABY = document.getElementById("KEBABY");
+    var ZAMOWIENIA = document.getElementById("ZAMOWIENIA");
+    var KONTAKT = document.getElementById("KONTAKT");
+    ZAMOWIENIA.classList.remove("none");
+    ZAMOWIENIA.classList.add("contentClass");
+
+    KEBABY.classList.remove("contentClass");
+    KEBABY.classList.add("none");
+
+    ONAS.classList.remove("contentClass");
+    ONAS.classList.add("none");
+
+    KONTAKT.classList.remove("contentClass");
+    KONTAKT.classList.add("none");
+}
+function KONTAKT()
+{
+    var ONAS = document.getElementById("ONAS");
+    var KEBABY = document.getElementById("KEBABY");
+    var ZAMOWIENIA = document.getElementById("ZAMOWIENIA");
+    var KONTAKT = document.getElementById("KONTAKT");
+    KONTAKT.classList.remove("none");
+    KONTAKT.classList.add("contentClass");
+
+    KEBABY.classList.remove("contentClass");
+    KEBABY.classList.add("none");
+
+    ZAMOWIENIA.classList.remove("contentClass");
+    ZAMOWIENIA.classList.add("none");
+
+    ONAS.classList.remove("contentClass");
+    ONAS.classList.add("none");
+}
