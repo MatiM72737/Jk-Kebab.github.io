4e062682e54c09578ac028163cb2f678a83a0060
MatiM72737 <72034316+MatiM72737@users.noreply.github.com>
2023-06-05 22:25:19 +0200

    cm

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 <html lang="pl">
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" href="/style.css"/>
+        <link rel="stylesheet" href="style.css"/>
         <title>JK Kebab</title>
         <meta name="description" content="Witamy na stronie JK Kebab - prawdziwej oazie smaku dla miłośników kebaba! Nasza restauracja położona jest w sercu miasta i specjalizuje się w serwowaniu autentycznych, świeżych i pysznych kebabów dla naszych gości. Na stronie JK Kebab znajdziesz wszystkie informacje, których potrzebujesz, aby poznać naszą historię, nasze menu i naszą pasję do kebabów. Przygotowaliśmy dla Ciebie wyjątkowy opis naszej restauracji, abyś mógł poczuć atmosferę i smak naszych dań zanim jeszcze do nas zawitasz. JK Kebab to miejsce, w którym staranne przygotowanie i wyjątkowe składniki łączą się w jednym wyjątkowym kawałku sztuki kulinarnej. Nasz kebab jest robiony na miejscu, na bieżąco, z najwyższą jakością mięsa i świeżych warzyw. Niezależnie od tego, czy wolisz tradycyjnego kebaba z wołowiną lub drobiem, czy może wolisz wegetariańską opcję, u nas zawsze znajdziesz coś dla siebie. Na stronie JK Kebab znajdziesz nasze pełne menu, wraz z opisami naszych różnorodnych kebabów i dodatków. Możesz zapoznać się z naszymi specjalnymi zestawami, które oferują smakowe kombinacje idealne dla Ciebie i Twojego towarzystwa. Pragniesz czegoś słodkiego? Oferujemy również pyszne desery, które idealnie dopełnią Twoje doświadczenie kulinarne. Jeśli szukasz wygodnego sposobu na zamówienie, na naszej stronie znajdziesz także opcję zamówienia online. Możesz z łatwością przeglądać nasze menu, wybrać swoje ulubione dania i złożyć zamówienie bez wychodzenia z domu. Jesteśmy tu po to, aby zapewnić Ci wygodę i zaspokoić Twoje kulinarne pragnienia. Na stronie JK Kebab znajdziesz również informacje o naszych aktualnych promocjach, wydarzeniach specjalnych i nowościach. Nasza restauracja to miejsce, w którym zawsze dzieje się coś ciekawego, więc warto być na bieżąco. Nie czekaj dłużej! Przejdź na stronę JK Kebab i zanurz się w świecie wyśmienitego smaku kebabów. Czekamy na Ciebie z otwartymi rękami i pysznymi daniami, które zaspokoją Twoje wszystkie kulinarne zachcianki. Do zobaczenia w JK Kebab!">
         <meta name="keywords" content="kebab, jan, krycha, tanio, dobrze, jk, ze smakiem, pyszne ciastka"/>
-        <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
-        <script src="/js/script.js"></script>
+        <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+        <script src="js/script.js"></script>
     </head>
     <body>
         <div class="container">
@@ -32,15 +32,98 @@
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3206.5437316138737!2d22.004079079345704!3d50.03823428220198!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473cfb023bbdddb7%3A0x6a382c7b1b21d313!2sJuliusza%20S%C5%82owackiego%202%2C%2035-060%20Rzesz%C3%B3w!5e1!3m2!1spl!2spl!4v1685970623981!5m2!1spl!2spl" width="450" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <div id="KEBABY" class="none">
-                    <h3>Nasze oferty</h3>
-                    <table border="1px">
-                        <tr><th>Rodzaj kebaba</th><th>Mięso do wyboru</th><th>Cena</th><th>Wielkość</th></tr>
-                        <tr><td>buła</td><td></td><td></td><td></td><td></td></tr>
-                        <tr><td>buła</td><td></td><td></td><td></td><td></td></tr>
-                        <tr><td>buła</td><td></td><td></td><td></td><td></td></tr>
-                        <td></td></tr>
-
-                    </table>
+                    <h1>Nasze oferty</h1>
+                    <?php  
+                        $host = 'db4free.net';
+                        $username = 'jk_root';
+                        $password = 'uZnvM42367';
+                        $database = 'jk_kebab';
+                        $conn = mysqli_connect($host, $username, $password, $database);
+                        
+                        // Sprawdzenie połączenia
+                        if (!$conn) {
+                            die("Błąd połączenia: " . mysqli_connect_error());
+                        }
+                        $sql = "SELECT nazwa,cena FROM miesa";
+                        $miesa = mysqli_query($conn, $sql);
+                        $sql = "SELECT nazwa,cena FROM opakowania";
+                        $opakowania = mysqli_query($conn, $sql);
+                        $sql = "SELECT nazwa,cena FROM sosy";
+                        $sosy = mysqli_query($conn, $sql);
+                    ?>
+                    <div id="nizsze">
+                        <div id="mieso">
+                            <h2>Mięsa</h2>
+                            <?php
+                            if ($miesa->num_rows > 0) {
+                                echo "<table>";
+                                echo "<tr><th>Nazwa</th><th>Cena</th></tr>";
+                                
+                                // Wyświetlanie danych o mięsie
+                                while ($row = $miesa->fetch_assoc()) {
+                                    $nazwa = $row["nazwa"];
+                                    $cena = $row["cena"];
+                                    
+                                    echo "<tr>";
+                                    echo "<td>$nazwa</td>";
+                                    echo "<td>$cena</td>";
+                                    echo "</tr>";
+                                }
+                                
+                                echo "</table>";
+                            } else {
+                                echo "Brak danych o mięsie.";
+                            }
+                            ?>
+                        </div>
+                        <div id="typ">
+                            <h2>Typ</h2>
+                            <?php
+                            if ($opakowania->num_rows > 0) {
+                                echo "<table>";
+                                echo "<tr><th>Nazwa</th><th>Cena</th></tr>";
+                                
+                                // Wyświetlanie danych o opakowaniach
+                                while ($row = $opakowania->fetch_assoc()) {
+                                    $nazwa = $row["nazwa"];
+                                    $cena = $row["cena"];
+                                    
+                                    echo "<tr>";
+                                    echo "<td>$nazwa</td>";
+                                    echo "<td>$cena</td>";
+                                    echo "</tr>";
+                                }
+                                
+                                echo "</table>";
+                            } else {
+                                echo "Brak danych o opakowaniach.";
+                            }?>
+                        </div>
+                        <div id="sos">
+                            <h2>Sosy</h2>
+                            <?php
+                            if ($sosy->num_rows > 0) {
+                                echo "<table>";
+                                echo "<tr><th>Nazwa</th><th>Cena</th></tr>";
+                                
+                                // Wyświetlanie danych o sosach
+                                while ($row = $sosy->fetch_assoc()) {
+                                    $nazwa = $row["nazwa"];
+                                    $cena = $row["cena"];
+                                    
+                                    echo "<tr>";
+                                    echo "<td>$nazwa</td>";
+                                    echo "<td>$cena</td>";
+                                    echo "</tr>";
+                                }
+                                
+                                echo "</table>";
+                            } else {
+                                echo "Brak danych o sosach.";
+                            }
+                            ?>
+                        </div>
+                    </div>
                 </div>
                 <div id="ZAMOWIENIA" class="none">Witamy na stronie JK Kebab - prawdziwej oazie smaku dla miłośników kebaba! Nasza restauracja położona jest w sercu miasta i specjalizuje się w serwowaniu autentycznych, świeżych i pysznych kebabów dla naszych gości.
 
